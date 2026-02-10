8b9841780fee8b67c55fc8d400b199dcbab87e68
Mithila Dilshan Bandara <mithila@synergysis.com>
2026-02-10 16:28:14 +0530

    added new changes

--- cypress.config.js
+++ cypress.config.js
@@ -2,10 +2,9 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: "http://192.168.62.238",
+    baseUrl: process.env.CYPRESS_BASE_URL || "http://192.168.62.238",
     setupNodeEvents(on, config) {
       return config;
-      // implement node event listeners here
     },
   },
 });
